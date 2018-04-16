818915ce9f12fde71eb792218d237961b469dc9f
Petar Beleganski <petar.beleganski@scalefocus.com>
2018-04-16 15:43:33 +0300

    Added demos for Enterprise Integration Patterns with Example and Unit Test for each one. // EDITED

--- src/main/java/com/my/camel/camel/enterprise/patterns/wiretap/WireTapRoute.java
+++ src/main/java/com/my/camel/camel/enterprise/patterns/wiretap/WireTapRoute.java
@@ -2,6 +2,14 @@ package com.my.camel.camel.enterprise.patterns.wiretap;
 
 import org.apache.camel.builder.RouteBuilder;
 
+/*
+ *   WireTap Enterprise Integration Pattern
+ *
+ *   We use it when we want to view sensitive data but we use external debug file with our data in order
+ *   not to pollute or harm the output result in the output folder
+ *
+ * */
+
 public class WireTapRoute  extends RouteBuilder{
 
     @Override
